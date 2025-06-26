80c1241c5e969317a2f86e5049caba234cd9e828
Leonardo <adil.silva.dev@gmail.com>
2025-06-26 08:11:37 -0300

    Start projeto 3a Fase

---
+++ Back-end/src/main/java/com/projeto/DevolvefacilApplication.java
@@ -0,0 +1,13 @@
+package com.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolvefacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevolvefacilApplication.class, args);
+	}
+
+}
